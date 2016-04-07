61e02e380ce8d4babcc5d720bae2f71b2678e62a
pimjager <pim@pimjager.nl>
2016-04-07 17:29:28 +0200

    Start with sylvan function

--- practical2/main.cpp
+++ practical2/main.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 #include <sylvan.h>
+#include <sylvan_obj.hpp>
+
+void setUpSylvan(){
+    lace_init(0, 0); //auto #workers and task_deque
+    lace_startup(0, NULL, NULL); //auto stack size
+    LACE_ME;
+    sylvan_init_package(1LL<<21, 1LL<<27, 1LL<<20, 1LL<<26);
+    sylvan_init_bdd(6);
+}
 
 int main(){
+    setUpSylvan();
     return 0;
 }
